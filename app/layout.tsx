70c19a322f423d45b367487c7e6ab672cfdda69a
Tom <infodeliveryswift@gmail.com>
2025-07-13 10:04:03 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+
+export const metadata: Metadata = {
+  title: "Delivery Swift",
+  description: "Delivery and Logistic company in the US",
+
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <ToastContainer />
+        <main className="bg-yellow-50/30">{children}</main>
+      </body>
+    </html>
+  );
+}
